b083a8cdce509fe1281a63baa8b897f414ba4c75
Priscilla Arowolo <priscillaamaka8@gmail.com>
2024-02-27 13:26:43 +0100

    Added basic html file and created div for the forecast weather. All wrapped inside different classes and targeted in css.

---
+++ index.js
@@ -0,0 +1,52 @@
+function replaceWeather (response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector ("#icon");
+cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = formatDate(date);
+iconElement.innerHTML = `<img src ="${response.data.condition.icon_url}"/>`;
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date){
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+if (hours < 10){
+    hours = `0${hours}`;
+}
+return `${day} ${hours}:${minutes}`
+}
+
+function searchCity (city){
+    let apiKey ="4b5at78o546f39c2e18ee9634f6ba403";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(replaceWeather);
+}
+
+
+
+function handleSearchSubmit (event){
+event.preventDefault();
+let searchInput = document.querySelector("#search-form-input");
+
+searchCity(searchInput.value);
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener ("submit", handleSearchSubmit);
+searchCity("Paris");
